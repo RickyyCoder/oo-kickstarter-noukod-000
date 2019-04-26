97bda250e6f28534cb348a12ac94bfdb00940c0d
Ricardo Paul <kingupper0@gmail.com>
2019-04-26 17:37:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
   ### the backer has his attribute (array) in Project 
   ### the backed_projects have their attribute (an array) in Backer
   
-##
+## to back a project ... the backer adds to project to his backed project array and to his own array backer
   
   def back_project(project)
        backed_projects << project
